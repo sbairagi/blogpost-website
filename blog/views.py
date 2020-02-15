dce9ff08a2497fc0a313afad67ae37c2045bcc48
shubham bairagi <sbairagi825@gmail.com>
2020-02-16 01:04:56 +0530

    create Home app or Blog app urls and render template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def bloghome(request):
+    return render(request,'blog/bloghome.html')
+
+def blogpost(request, slug):
+    
+    return render(request,'blog/blogpost.html')
